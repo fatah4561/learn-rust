610647f715b644a472bf420f53c4394dfbf2d32a
fatah4561 <fatahathariq6@gmail.com>
2024-08-02 16:28:01 +0700

    group import in main

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@
 mod modules;
 
 #[allow(unused_imports)]
-use modules::a_hello_world::ab_formatted_print::formatted_display;
-use modules::b_primitives::a_literals_and_operators::a_literals_and_operators;
-
+use modules::{
+    a_hello_world::ab_formatted_print::formatted_display,
+    b_primitives::a_literals_and_operators::a_literals_and_operators,
+};
 
 fn main() {
     // formatted_display();
-    a_literals_and_operators()
-
+    // a_literals_and_operators()
 }
